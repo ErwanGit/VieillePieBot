847a79c335a27e90a2d709a7191afb68b92f4402
Erwan <contact@erwan2.fr>
2024-05-28 10:42:10 +0200

    chore(deps): Upgrade deps

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,22 +2,20 @@ import { Client, IntentsBitField } from 'discord.js';
 import MessageTodayManager from './modules/MessageToday';
 
 export default class VieillePieBot extends Client {
-    public msgTodayManager: MessageTodayManager;
+  public msgTodayManager: MessageTodayManager;
 
-    constructor() {
-        super({
-            presence: {
-                status: 'online'
-            },
-            intents: IntentsBitField.Flags.Guilds | IntentsBitField.Flags.GuildMembers
-        });
+  constructor() {
+    super({
+      presence: { status: 'online' },
+      intents: IntentsBitField.Flags.Guilds | IntentsBitField.Flags.GuildMembers
+    });
 
-        this.msgTodayManager = new MessageTodayManager(this);
+    this.msgTodayManager = new MessageTodayManager(this);
 
-        this.once('ready', (client) => {
-            console.log(`[${client.user.username}] Bot connecté !`);
+    this.once('ready', (client) => {
+      console.log(`[${client.user.username}] Bot connecté !`);
 
-            this.msgTodayManager.init();
-        });
-    }
+      this.msgTodayManager.init();
+    });
+  }
 }
